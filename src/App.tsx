d93163512a4b9ef4eeb29b2b465558c9c6a08ef0
Belar <belar@belardesign.com>
2024-06-27 16:08:45 +0000

    feat: add example app content

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,52 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+/**
+* This is a basic example of use of monday.com SDK and Vibe UI components.
+ * 
+ * Based on https://github.com/mondaycom/welcome-apps/blob/master/apps/quickstart-react/src/App.js
+ */
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useState, useEffect } from "react";
+
+import mondaySdk from "monday-sdk-js";
+import { BaseContext } from "monday-sdk-js/types/client-context.type";
+
+import "monday-ui-react-core/dist/main.css";
+//Explore more Monday React Components here: https://style.monday.com/
+import { AttentionBox } from "monday-ui-react-core";
+
+import "./App.css";
+
+// Usage of mondaySDK example, for more information visit here: https://developer.monday.com/apps/docs/introduction-to-the-sdk/
+const monday = mondaySdk();
+
+const App = () => {
+  const [context, setContext] = useState<BaseContext>();
+
+  useEffect(() => {
+    // Notice this method notifies the monday platform that user gains a first value in an app.
+    // Read more about it here: https://developer.monday.com/apps/docs/mondayexecute#valuecreatedforuser
+    monday.execute("valueCreatedForUser");
+
+    // TODO: set up event listeners, Here`s an example, read more here: https://developer.monday.com/apps/docs/mondaylisten/
+    monday.listen("context", (res) => {
+      setContext(res.data);
+    });
+  }, []);
+
+  //Some example what you can do with context, read more here: https://developer.monday.com/apps/docs/mondayget#requesting-context-and-settings-data
+  const attentionBoxText = `Hello, your user_id is: ${
+    context ? context.user.id : "still loading"
+  }.
+  Let's start building your amazing app, which will change the world!`;
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+    <div className="App">
+      <AttentionBox
+        title="Hello Monday Apps!"
+        text={attentionBoxText}
+        type={AttentionBox.types.SUCCESS}
+      />
+    </div>
+  );
+};
 
-export default App
+export default App;
